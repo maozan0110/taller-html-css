13f115cfda8a45a638078159e40fa43a79c1aaf3
manuel ospina <maospina59925@umanizales.edu.co>
2021-02-28 17:29:02 -0500

    initial vertion

---
+++ Actualidad/admitidos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"content="ie=edge">
+    <title>Resultado de busqueda</title>
+    <link rel="stylesheet" type="text/css" href="csshtml/css/styles.css"/>
+</head>
+<body>
+<h1> 
+    Aun no estas dentro del primer listado de ciudadanos a vacunar
+<br>debes esperar que tu EPS se comunique contigo para validar fecha de agenda</br>
+
+</h1>
+
+<img src="imagenes/covid.jpg">
+</body>
+</html>
